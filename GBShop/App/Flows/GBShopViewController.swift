162a0d98b10600425f762baecdbaaf98e926698e
Jarek Šedý <jareksedy@icloud.com>
2021-12-01 10:55:51 +0600

    Added GBShopViewController and linked it to GBShop.storyboard.

---
+++ GBShop/App/Flows/GBShopViewController.swift
@@ -0,0 +1,17 @@
+//
+//  GBShopViewController.swift
+//  GBShop
+//
+//  Created by Ярослав on 01.12.2021.
+//
+
+import UIKit
+
+class GBShopViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print("Hello from GBShop.")
+    }
+}
