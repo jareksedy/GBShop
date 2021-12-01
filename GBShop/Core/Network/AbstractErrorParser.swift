c9149f9e014cea11c0e28885551d07a368cc08b9
Jarek Šedý <jareksedy@icloud.com>
2021-12-01 11:50:46 +0600

    Added AbstractErrorParser.swift.

---
+++ GBShop/Core/Network/AbstractErrorParser.swift
@@ -0,0 +1,23 @@
+//
+//  AbstractErrorParser.swift
+//  GBShop
+//
+//  Created by Ярослав on 01.12.2021.
+//
+
+import Foundation
+
+protocol AbstractErrorParser {
+    func parse(_ result: Error) -> Error
+    func parse(response: HTTPURLResponse?, data: Data?, error: Error?) -> Error?
+}
+
+class ErrorParser: AbstractErrorParser {
+    func parse(_ result: Error) -> Error {
+        return result
+    }
+    
+    func parse(response: HTTPURLResponse?, data: Data?, error: Error?) -> Error? {
+        return error
+    }
+}
