127adc2521cdd365a690fcebac07674b809a220d
Jarek Šedý <jareksedy@icloud.com>
2022-01-15 21:44:09 +0600

    Added back button on cart screen + changed toolbar item color to orange.

--- GBShop/App/Flows/Cart/CartTableViewController.swift
+++ GBShop/App/Flows/Cart/CartTableViewController.swift
@@ -12,6 +12,20 @@ class CartTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setToolbarHidden(false, animated: true)
+        navigationController?.setNavigationBarHidden(false, animated: true)
+        navigationController?.navigationBar.topItem?.hidesBackButton = false
+        navigationController?.navigationBar.isTranslucent = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+//        navigationController?.setToolbarHidden(true, animated: false)
+//        navigationController?.setNavigationBarHidden(true, animated: false)
+    }
 
     // MARK: - Table view data source
 
@@ -21,5 +35,15 @@ class CartTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if AppCart.shared.items.count == 0 {
+            let cell = UITableViewCell()
+            cell.textLabel?.text = "Корзина пуста"
+            return cell
+        } else {
+            return UITableViewCell()
+        }
     }
 }
